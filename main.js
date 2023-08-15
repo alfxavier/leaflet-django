813bb0656decbc3f72311f5f9486a8e8f1df0c12
alfxavier <alfxavier@gmail.com>
2023-08-14 21:43:51 -0300

    sobreposição com imagens

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 window.onload = init;
 
 function init() {
-
+    //Elemento HTML
     const mapElement = document.getElementById('mapid')
 
+
+    //BaseMaps
     const sateliteMaps = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     })
@@ -13,6 +15,8 @@ function init() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })
 
+
+    // Leaflet map object
     const mymap = L.map(mapElement, {
         center: [-15.839443634001434, -48.02992505486712],
         zoom: 19,
@@ -20,11 +24,27 @@ function init() {
         layers: [sateliteMaps]
     })
 
+    // Basemap Object
     const baseLayers = {
         '<b>Tiles Esri</b>' : sateliteMaps,
         'Open Street Map' : openStreetMap
     }
-    const layerControls = L.control.layers(baseLayers, {}, {
+
+    // Overlays
+    const uniplanr22BaseMapImage = './data/uniplan_rua22.png';
+    const uniplanr22BaseMapBounds = [[-15.839789426551249, -48.027527332305915],[-15.83696649081145, -48.0251830816269]]
+    const imageuniplanr22Overlay = L.imageOverlay(uniplanr22BaseMapImage, uniplanr22BaseMapBounds)
+
+    const OverlayerLayers = {
+      'Rua 22 N x Uniplan': imageuniplanr22Overlay  
+    }    
+
+    mymap.on('click', function(event){
+        console.log(event.latlng)
+    })
+
+    // Layer Control
+    const layerControl = L.control.layers(baseLayers, OverlayerLayers, {
         collapsed: false
     }).addTo(mymap)
 
